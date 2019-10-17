2c6ac87b490c51673f15565c5fd0df6810bcc63b
AbigailFernandes <abigailferns94@gmail.com>
2019-10-17 17:17:26 -0600

    Add gitignore

--- build/real-sense-wrapper/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/odroid/project/src/real-sense-wrapper
-# Build directory: /home/odroid/project/build/real-sense-wrapper
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
