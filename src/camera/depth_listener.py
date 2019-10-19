25c7d1b8cb2088a0913f1a63e570aa389a84e605
AbigailFernandes <abigailferns94@gmail.com>
2019-10-19 17:26:51 -0600

    Add publisher for depth

--- src/camera/depth_listener.py
+++ src/camera/depth_listener.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Image as msg_Image
+from camera.msg import *
 from cv_bridge import CvBridge, CvBridgeError
-
 
 class ImageListener:
     def __init__(self, topic):
@@ -10,19 +10,23 @@ class ImageListener:
         self.corners = self.get_corners(self.height_dim, self.width_dim, 240, 320)
         self.topic = topic
         self.bridge = CvBridge()
+        self.rate = rospy.Rate(10) # 10hz
+        self.pub = rospy.Publisher('depth_frames', Depth, queue_size=100)
         self.sub = rospy.Subscriber(topic, msg_Image, self.imageDepthCallback)
+        self.msg = Depth()
 
     def imageDepthCallback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
             pix = (data.width/2, data.height/2)
-            print(data.width, data.height)
-            left_depth = self.getAverageDepth(cv_image, self.width_dim, self.height_dim, self.corners[0], self.corners[1])
-            cen_depth = self.getAverageDepth(cv_image, self.width_dim, self.height_dim, self.corners[2], self.corners[3])
-            right_depth = self.getAverageDepth(cv_image, self.width_dim, self.height_dim, self.corners[4], self.corners[5])
+            msg.left_depth = self.getAverageDepth(cv_image, self.width_dim, self.height_dim, self.corners[0], self.corners[1])
+            msg.center_depth = self.getAverageDepth(cv_image, self.width_dim, self.height_dim, self.corners[2], self.corners[3])
+            msg.right_depth = self.getAverageDepth(cv_image, self.width_dim, self.height_dim, self.corners[4], self.corners[5])
             print("Left Depth: ", left_depth)
             print("Center Depth: ", center_depth)
             print("Right Depth: ", right_depth)
+            self.pub.publish(msg)
+            self.rate.sleep()
             # cen_depth = cv_image[pix[1], pix[0]]
             # if depth_at_cen
             # print('%s: Depth at center(%d, %d): %f(mm)\r' % (self.topic, pix[0], pix[1], cv_image[pix[1], pix[0]]))
@@ -63,5 +67,5 @@ def main():
 
 if __name__ == '__main__':
     node_name = 'depth_listener'
-    rospy.init_node(node_name)
+    rospy.init_node(node_name, anonymous=True)
     main()
