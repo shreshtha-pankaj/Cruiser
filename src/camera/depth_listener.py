22c75d6866e282925a089b26d36f3e7e0f2f9f53
AbigailFernandes <abigailferns94@gmail.com>
2019-10-19 14:55:54 -0600

    Include env

--- src/camera/depth_listener.py
+++ src/camera/depth_listener.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Image as msg_Image
 from cv_bridge import CvBridge, CvBridgeError
-import sys
-import os
+
 
 class ImageListener:
     def __init__(self, topic):
@@ -14,8 +14,7 @@ class ImageListener:
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
             pix = (data.width/2, data.height/2)
-            sys.stdout.write('%s: Depth at center(%d, %d): %f(mm)\r' % (self.topic, pix[0], pix[1], cv_image[pix[1], pix[0]]))
-            sys.stdout.flush()
+            print('%s: Depth at center(%d, %d): %f(mm)\r' % (self.topic, pix[0], pix[1], cv_image[pix[1], pix[0]]))
         except CvBridgeError as e:
             print(e)
             return
@@ -26,6 +25,7 @@ def main():
     rospy.spin()
 
 if __name__ == '__main__':
+    print('Hi')
     node_name = 'camera_depth'
     rospy.init_node(node_name)
     main()
