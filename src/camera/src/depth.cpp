b50526a87bca7b6c09cb66b3f3f870a9e38952df
AbigailFernandes <abigailferns94@gmail.com>
2019-10-26 17:15:22 -0600

    Add Left, Right depth

--- src/camera/src/depth.cpp
+++ src/camera/src/depth.cpp
@@ -4,9 +4,43 @@
 #include <camera/Depth.h>
 #include <librealsense2/rs.hpp>
 
+float getAverageDepth(rs2::depth_frame depth, float width, float height, int x, int y) {
+  float sum = 0;
+  int ctr = 0;
+  for(int i = y; i < y + height; i++) {
+    for (int j = x; j < x + width; j++) {
+      float depth = depth.get_distance(x, y);
+      if depth > 0 {
+        ctr += 1;
+        sum += depth;
+      }
+    }
+  }
+  if ctr == 0 {
+    return 0;
+  }
+  return (sum / ctr)
+}
+
+float* getCorners(float width, float height, int cx, int cy) {
+  float *corners = new float[6];
+  corners[0] = 0;
+  corners[1] = cy - height / 2;
+  corners[2] = cx - width / 2;
+  corners[3] = cy - height / 2;
+  corners[4] = 640 - width;
+  corners[5] = cy - height / 2;
+  return corners
+}
+
 int main(int argc, char **argv){
   ros::init(argc, argv, "depth_stream");
   ros::NodeHandle n;
+  int width = 100;
+  int height = 100;
+  int cx = 320;
+  int cy = 240;
+  float* corners = getCorners(width, height, cx, cy);
 
   // Create a publisher node
   ros::Publisher depth_pub = n.advertise<camera::Depth>("depth_frames", 1000);
@@ -30,12 +64,13 @@ int main(int argc, char **argv){
       // Get the depth frame's dimensions
       float width = depth.get_width();
       float height = depth.get_height();
+      ROS_INFO("Width:%f, Height:%f\n", width, height);
 
       // Query the distance from the camera to the object in the center of the image
-      float dist_to_center = depth.get_distance(width / 2, height / 2);
-      msg.center_depth = dist_to_center;
-      // msg.left_depth = ;
-      // msg.right_depth = ;
+      // float dist_to_center = depth.get_distance(width / 2, height / 2);
+      msg.left_depth = getAverageDepth(depth, width, height, corners[0], corners[1]);
+      msg.center_depth = getAverageDepth(depth, width, height, corners[0], corners[1]);
+      msg.right_depth = getAverageDepth(depth, width, height, corners[0], corners[1]);
       // Print the distance
 
       // Publish the message
