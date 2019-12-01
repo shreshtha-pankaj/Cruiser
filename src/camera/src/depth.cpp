77a6187d454e9b72afb484d271ec1ecec26bc444
AbigailFernandes <abigailferns94@gmail.com>
2019-12-01 16:32:35 -0700

    Increase frame rate

--- src/camera/src/depth.cpp
+++ src/camera/src/depth.cpp
@@ -4,8 +4,8 @@
 #include <camera/Depth.h>
 #include <librealsense2/rs.hpp>
 
-int depth_width = 1280;
-int depth_height = 720;
+int depth_width = 640;
+int depth_height = 480;
 
 float getAverageDepth(rs2::depth_frame& depth, float width, float height, int x, int y) {
   float sum = 0;
@@ -53,7 +53,7 @@ int main(int argc, char **argv){
   rs2::pipeline p;
   // Configure and start the pipeline
   rs2::config config;
-  config.enable_stream(RS2_STREAM_DEPTH, depth_width, depth_height, RS2_FORMAT_Z16, 30);
+  config.enable_stream(RS2_STREAM_DEPTH, depth_width, depth_height, RS2_FORMAT_Z16, 60);
   p.start(config);
   while (ros::ok())
   {
