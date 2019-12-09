7504371c6dec8a928c3e3e1e2f2eaea423774917
AbigailFernandes <abigailferns94@gmail.com>
2019-12-09 13:18:46 -0700

    Add Ball Detection Code

--- src/ball_detection/src/blob_detector.py
+++ src/ball_detection/src/blob_detector.py
@@ -4,12 +4,21 @@ import cv2
 import numpy as np;
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
- 
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+
+cyanLower = (60, 50, 100)
+cyanUpper = (180, 255, 255)
 
 class BlobDetector:
     def __init__(self):
         rgb_topic = '/camera/rgb'
         depth_topic = '/camera/depth'
+
         # Set up the detector with default parameters.
         self.detector = cv2.SimpleBlobDetector()
         self.pub = rospy.Publisher('ball_detected', Bool, queue_size=50)
@@ -19,26 +28,49 @@ class BlobDetector:
     
     def rgb_callback(self, data):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data,"mono8")
-            # cv_image = cv2.resize(cv_image,(253,200))
-            # Detect blobs.
-            keypoints = detector.detect(im)
-            rospy.loginfo('Keypoints in image, ', keypoints)
-            # Draw detected blobs as red circles.
-            # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-            im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)         
-            # Show keypoints
-            cv2.imshow("Keypoints", im_with_keypoints)
-            cv2.waitKey(0)
+            #  check if it should be bgr8 or rgb8
+            frame = self.bridge.imgmsg_to_cv2(data,"bgr8")
+            
+            # frame = imutils.resize(frame, width=600)
+            blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-            if len(keypoints) > 0:
-                if self.is_ball == False:
-                    self.is_ball = True
-                    self.pub.publish(self.is_ball)
-            else:
-                if self.is_ball:
-                    self.is_ball = False
-                    self.pub.publish(self.is_ball)
+            mask = cv2.inRange(hsv, cyanLower, cyanUpper)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)
+            cnts = imutils.grab_contours(cnts)
+            center = None
+
+            # only proceed if at least one contour was found
+            if len(cnts) > 0:
+                c = max(cnts, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+                # only detect ball if the radius meets a minimum size
+                if radius > 10:
+                    cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+                    cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                    # publish only while transitioning from ball detected to not
+                    if self.is_ball == False:
+                        self.is_ball = True
+                        self.pub.publish(self.is_ball)
+                        rospy.loginfo("Ball Detection: We see a ball rolling towards us")
+                else:
+                    if self.is_ball:
+                        self.is_ball = False
+                        self.pub.publish(self.is_ball)
+                        rospy.loginfo("Ball Detection: We don't see a ball anymore")
+
+            # Removed tracking code as we don't need it
+          
+            # show the frame to our screen
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
         except CvBridgeError as e:
             rospy.logerr(str(e))
         
