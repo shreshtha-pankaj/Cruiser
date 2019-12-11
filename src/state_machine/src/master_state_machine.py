64dde2c93dec4070ef6588021cc81ab99e91cada
shreshtha-pankaj <spankaj@splunk.com>
2019-12-10 17:12:08 -0700

    cherry picking code which directly publishes to pololu instead of action client

--- src/state_machine/src/master_state_machine.py
+++ src/state_machine/src/master_state_machine.py
@@ -32,8 +32,9 @@ class Straight(State):
 
     def move(self, state_machine, servo=0, motor =slow_motor):
         self.state = 'straight'
-        state_machine.create_trajectory_Motor_cmd('servo', servo)
-        state_machine.create_trajectory_Motor_cmd('brushless_motor', motor)
+        #state_machine.create_trajectory_Motor_cmd_2([servo,motor])
+        state_machine.create_trajectory_Motor_cmd_3(state_machine.pololu.joint_name_servo, servo)
+        state_machine.create_trajectory_Motor_cmd_3(state_machine.pololu.joint_name_brushless_motor, motor)
 
 
 class Right(State):
@@ -47,17 +48,9 @@ class Right(State):
         print("Depth while turning:left, center, right ", state_machine.left_depth,state_machine.center_depth,state_machine.right_depth)
         while time.time() < end_time:
             self.state = 'right'
-            state_machine.create_trajectory_Motor_cmd('servo', servo)
-            state_machine.create_trajectory_Motor_cmd('brushless_motor', motor)
-
-        self.center_depth > turn_depth and self.turn_state_flag:
-            curr_time = time.time()
-            while time.time() - curr_time < 0.8:
-                self.straight.move(self,servo=0.5,motor=0)
-            while time.time() - curr_time < 2:
-                self.straight.move(self,servo=self.pid_value,motor=-0.4)
-            self.turn_state_flag = False
-
+            state_machine.create_trajectory_Motor_cmd_3(state_machine.pololu.joint_name_servo, servo)
+            state_machine.create_trajectory_Motor_cmd_3(state_machine.pololu.joint_name_brushless_motor, motor)
+            #state_machine.create_trajectory_Motor_cmd_2([servo,motor])
 
 class Stop(State):
 
@@ -65,25 +58,27 @@ class Stop(State):
         self.state_name = state_name
         self.reverse_flag= True
 
-    def stop(self, state_machine, servo=servo_zero, motor=stop_motor):
-        state_machine.create_trajectory_Motor_cmd('servo',servo)
-        state_machine.create_trajectory_Motor_cmd('brushless_motor', motor)
+    def stop(self, state_machine, servo=servo_zero, motor=reverse_motor):
+        #state_machine.create_trajectory_Motor_cmd_2([servo,motor])
+        state_machine.create_trajectory_Motor_cmd_3(state_machine.pololu.joint_name_servo, servo)
+        state_machine.create_trajectory_Motor_cmd_3(state_machine.pololu.joint_name_brushless_motor, motor)
         if self.reverse_flag:
-            state_machine.create_trajectory_Motor_cmd('brushless_motor', reverse_motor)
+            state_machine.create_trajectory_Motor_cmd_3(state_machine.pololu.joint_name_brushless_motor, reverse_motor)
+            # state_machine.create_trajectory_Motor_cmd('brushless_motor', reverse_motor)
             self.reverse_flag= False
             time.sleep(0.5)
-        state_machine.create_trajectory_Motor_cmd('brushless_motor', motor)
+        state_machine.create_trajectory_Motor_cmd_3(state_machine.pololu.joint_name_brushless_motor, motor)
+        # state_machine.create_trajectory_Motor_cmd('brushless_motor', motor)
 
 
 class StateMachine(object):
 
 
     def __init__(self, pub_topic, sub_topic_depth, sub_topic_pid, sub_topic_stop_sign):
-        self.client = actionlib.SimpleActionClient('pololu_trajectory_action_server', pololu_trajectoryAction)
         self.sub_depth = rospy.Subscriber(sub_topic_depth, Depth, callback=self.sub_depth_callback)
         self.sub_pid = rospy.Subscriber(sub_topic_pid, Float32, callback=self.sub_pid_callback)
         self.sub_stop_sign = rospy.Subscriber(sub_topic_stop_sign, Bool, callback=self.sub_stop_sign_callback)
-        #self.sub_turn_depth = rospy.Subscriber(sub_topic_depth, Depth, callback=self.sub_depth_turn_callback)
+        self.pololu = Polulu_Command()
         self.center_depth, self.left_depth, self.right_depth = 0, 0, 0
         self.straight = Straight("Move-Straight")
         self.right = Right("Move-Right")
@@ -100,39 +95,6 @@ class StateMachine(object):
         self.center_depth = data.center_depth
         self.left_depth = data.left_depth
         self.right_depth = data.right_depth
-        if self.start_flag:
-            tim = time.time()
-            while(time.time() - tim < 1.0):
-                self.straight.move(self, servo=0.08, motor=-0.55)
-            self.start_flag = False
-
-
-        if self.center_depth > turn_depth and not self.turn_state_flag:
-            print('Car is moving straight(l, c, r)', self.left_depth, self.center_depth, self.right_depth)
-            self.straight.move(self,servo = self.pid_value,motor=high_speed)
-
-        # TODO: Explain what is happening here
-        elif self.center_depth > turn_depth and self.turn_state_flag:
-            curr_time = time.time()
-            while time.time() - curr_time < 0.8:
-                self.straight.move(self,servo=0.5,motor=0)
-            while time.time() - curr_time < 2:
-                self.straight.move(self,servo=self.pid_value,motor=-0.4)
-            self.turn_state_flag = False
-
-        # Too close to an obstacle, STOP
-        elif self.center_depth < 1200:
-            #stop the car for now.
-            #print('Car is stopping', self.center_depth)
-            self.stop.stop(self)
-
-        elif data.center_depth < turn_depth: # and data.center_depth > 5500:# and self.center_depth > 5500:# and not iself.turn_flag:
-            self.is_in_turn = True
-            #if self.center_depth > 6000:
-            print('Car is turning now', data.center_depth)
-            #self.straight.move(self, servo=self.pid_value, motor=0.8)
-            self.right.turn(self)
-            self.turn_state_flag = True
 
     def sub_pid_callback(self, data):
         self.pid_value = data.data
@@ -150,6 +112,26 @@ class StateMachine(object):
         self.client.send_goal(goal)
         self.client.wait_for_result(rospy.Duration.from_sec(3.0))
 
+    def create_trajectory_Motor_cmd_2(self, pos, speed=0):
+        T0 = time.time()
+        goal = pololu_trajectoryGoal()
+        traj = goal.joint_trajectory
+        traj.header.stamp = rospy.Time.now()
+        traj.joint_names.append('servo')
+        traj.joint_names.append('brushless_motor')
+        pts = JointTrajectoryPoint()
+        pts.time_from_start = rospy.Duration(0.0)
+        pts.positions.append(float(pos[0]))
+        pts.positions.append(float(pos[1]))
+        pts.velocities.append(speed)
+        pts.velocities.append(speed)
+        traj.points.append(pts)
+        self.client.send_goal(goal)
+        self.client.wait_for_result(rospy.Duration.from_sec(3.0))
+
+    def create_trajectory_Motor_cmd_3(self,jnt_name, pos, speed=0):
+        self.pololu.send_command(jnt_name, pos, speed)
+
     def sub_stop_sign_callback(self, data):
         self.is_stop_sign = data.data
 
@@ -166,7 +148,7 @@ class StateMachine(object):
 
         # move straight when we above a certain depth threshold and not in the turning state
         if self.center_depth > turn_depth and not self.turn_state_flag:
-            print('Car is moving straight(l, c, r)', self.left_depth, self.center_depth, self.right_depth)
+            #rospy.loginfo('Car is moving straight(l, c, r): %f, %f, %f', self.left_depth, self.center_depth, self.right_depth)
             self.straight.move(self,servo = self.pid_value,motor=high_speed)
 
         # TODO: Explain what is happening here
@@ -185,22 +167,28 @@ class StateMachine(object):
             self.stop.stop(self)
 
         # Car is turning right
-        #elif self.center_depth < 6000:# and self.center_depth > 5500:# and not iself.turn_flag:
-            #if self.center_depth > 6500:
-            #print('Car is slowing down', self.center_depth)
-            #self.straight.move(self, servo=self.pid_value, motor=0.8)
-            #time.sleep(0.25)
-            #while self.center_depth > 5000 and self.right_depth < 3000:
-            #if self.right_depth < 3000:
-            #else:
-            #self.right.turn(self)
-            #self.right.turn(self)
-            #self.turn_flag = True
-            #self.turn_state_flag = True
+        elif self.center_depth < 6000:# and not iself.turn_flag:
+            self.right.turn(self)
+            self.turn_state_flag = True
 
        #elif self.center_depth < 5500:# and self.right_depth > 3000:# and self.turn_flag:
             #self.right.turn(self)
 
+
+
+class Polulu_Command:
+    def __init__(self):
+        self.joint_name_servo = 'servo'
+        self.joint_name_brushless_motor = 'brushless_motor'
+        self.pub=rospy.Publisher("/pololu/command",MotorCommand,queue_size=10)
+    
+    def send_command(self, jntName, pos, speed=0):
+        mtr=MotorCommand()
+        mtr.joint_name=jntName
+        mtr.position=pos
+        mtr.speed=speed#/self.MaxSpeed#pololu take 0 to 1.0 as speed, check the correct division
+        mtr.acceleration=1.0
+        self.pub.publish(mtr)
 
 
 if __name__ =='__main__':
@@ -210,8 +198,8 @@ if __name__ =='__main__':
     pub_topic = '/car_state'
     rospy.init_node('car_state_pub')
     ss = StateMachine(pub_topic, sub_topic_depth,sub_topic_pid,sub_topic_stop_sign)
-    ss.client.wait_for_server()
-    print('State Machine Started')
+    rospy.loginfo('Initializing Master State Machine')
+#    ss.initial_run()
     while not rospy.is_shutdown():
         pass
         # if True:#not ss.is_in_turn:
